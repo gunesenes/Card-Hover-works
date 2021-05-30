b5a811544f5e710b2daac24a58230de1b2f641c9
Enes güneş <gunesenes.38@gmail.com>
2021-05-30 16:43:50 +0300

    Add files via upload

---
+++ card-hover-effect-title/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.card').on('mouseenter',function(e){
+        x = e.pageX - $(this).offset().left,
+        y = e.pageY - $(this).offset().top;
+        $(this).find('span').css({top:y, left:x})
+    })
+    $('.card').on('mouseout',function(e){
+        x = e.pageX - $(this).offset().left,
+        y = e.pageY - $(this).offset().top;
+        $(this).find('span').css({top:y, left:x})
+        })
+    })
